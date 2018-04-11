158a733a3398dc6597547dbdaddd7e2d19e013ec
fpe <wdxsbmc@gmail.com>
2018-04-11 17:44:04 +0800

    degbug ok

--- com.py
+++ com.py
@@ -2,10 +2,11 @@ import os
 # pip3 install pyserial
 import serial
 from serial import *
+import time
 
 class com():
 
-    message='' 
+    message = [] 
 
     def __init__(self):
         '''
@@ -36,13 +37,14 @@ class com():
         return number 
 
     def read_data(self):  
-        #while True:  
-        data=self.ser.read_all()
-        self.message.extend(data)
+        n = self.ser.inWaiting()
+        if(n > 0):
+            data=self.ser.read(n)
+            self.message.extend(data)
 
     '''
         68H	Addr	SubAddr	07H	07H	CUR_MOD	Freq	TotalS	CS	16H
-    test:  FE FE FE FE 68 01 FF 07 07 00 E8 03 05 00 00 00 66 16
+    test:  FE FE FE FE 68 01 01 07 07 00 E8 03 05 00 00 00 68 16
     '''
     def make_packet(self,board_num,board_port,ctl_code,data_len,data):
     
@@ -65,7 +67,7 @@ class com():
 
         # udpate checksum
         if(len(pkt) > 0):
-            for i in range(0,len(pkt)):
+            for i in range(4,len(pkt)):
                 checksum = checksum + int(pkt[i])
 
         pkt.append(checksum%256)
@@ -73,6 +75,16 @@ class com():
         return pkt
 
 
+    def cmd_send(self,board_num,board_port,ctl_code,data_len,data,exp_code):
+
+        if(self.ser.is_open):
+            cmd = self.make_packet(board_num, board_port, ctl_code, data_len, data)
+            #   FE FE FE FE 68 01 01 07 07 00 E8 03 05 00 00 00 68 16
+            print(cmd)
+
+            self.send_data(cmd)
+        return 1
+        
 
     def cmd_send_recv(self,board_num,board_port,ctl_code,data_len,data,exp_code):
      
@@ -81,18 +93,35 @@ class com():
 
             cmd = self.make_packet(board_num, board_port, ctl_code, data_len, data)
             
-            # cmd = [0x01, 0x05, 0x91, 0xF5, 0x00, 0x00, 0xF1, 0x04]
+
+            #   FE FE FE FE 68 01 01 07 07 00 E8 03 05 00 00 00 68 16
             print(cmd)
 
             self.send_data(cmd)
+
+            time.sleep(1)
+
             self.read_data()
 
-            recv_data = self.message[0:7]       
+            recv_data = self.message[0:11]       
+
+
 
             barr = bytearray(recv_data)
 
-            if(barr[4] == exp_code):
+            if(barr.count == 0):
+                return 0
+            if(barr[7] == exp_code):
                 return 1
             else:
                 return 0
 
+    # 转成16进制的函数
+    def convert_hex(self, string):
+        res = []
+        result = []
+        for item in string:
+            res.append(item)
+        for i in res:
+            result.append(hex(i))
+        return result
--- dc_current_test.py
+++ dc_current_test.py
@@ -6,6 +6,8 @@ from tkinter import *
 import com
 from com import *
 import time
+import _thread
+
 
 test_run = 0
 
@@ -13,12 +15,84 @@ root = tkinter.Tk()
 root.title("DC Current TestV1.0.0")
 SERIAL = com()
 
+# read save data thread
+def read_data_thd(SERIAL, test_run):
+    time.sleep(1)
+    while test_run:       
+        SERIAL.read_data()
+        time.sleep(1)
+
+
+def parse_save_thd(SERIAL, test_run):
+
+    pkt_offset = 0 
+    time.sleep(1)  
+    old_len = 0
+    old_offset = 0
+    parse_msg = SERIAL.message[old_len:]
+    b_find = 0
+
+    while test_run:       
+        
+        # get pkt
+        parse_msg.extend(SERIAL.message[old_len:])
+        old_len = len(parse_msg)
+
+        # parse message
+        while(pkt_offset < len(parse_msg)): 
+
+            # wait next pkt or stop
+            if(pkt_offset + 4 > len(parse_msg)):
+                if(old_offset != pkt_offset):
+                    #print('break at:',pkt_offset)
+                    old_offset = pkt_offset
+                break
+
+            # find pkt head
+            print(pkt_offset,len(parse_msg))
+            b_find = 0
+            for i in range(pkt_offset,len(parse_msg)):
+                if(parse_msg[i - 1] == 0xFE and parse_msg[i] == 0x68):
+                    pkt_offset = i #move offset
+                    b_find = 1
+                    break
+            if(b_find == 0):
+                break
+
+            # get pkt data len
+            print(pkt_offset)
+            pkt_data_len =  parse_msg[pkt_offset + 4]
+            if(pkt_data_len == 0):
+                pkt_offset = pkt_offset + 6  #skip first rsp
+                continue
+
+            # get pkt data
+            pkt_data_len = pkt_data_len - 5  #skip Seq(4bytes)+CUR_MOD(1byte)    
+            pkt_data = parse_msg[pkt_offset+9:pkt_offset+9+pkt_data_len]
+            pkt_offset = pkt_offset + 9 + pkt_data_len 
+            print(pkt_data)
+
+            # save data  
+            with open(".\dc_current_data.txt",'a') as fp:   
+                for i in range(0,len(pkt_data),2):
+                    #dcL dcH =>int16
+                    dc_value = 0
+                    dc_value = pkt_data[i+1] 
+                    dc_value <<= 8 
+                    dc_value = dc_value | pkt_data[i]
+                    fp.write(str(dc_value) + ',')  
+
+
+            
+
+        
 
 # button sttart
 def button_start():
     print("test start>>>")
-    # init com
 
+               
+    # init com
     SERIAL.init_com('com' + en_com.get())
     SERIAL.port_open()
 
@@ -40,45 +114,30 @@ def button_start():
     data.append(n_time & 0xFF)
 
     data_len = len(data)
-    ret = SERIAL.cmd_send_recv(en_pld_borad.get(),en_pld_port.get(),0x07,data_len,data,0x87)
+    ret = SERIAL.cmd_send(en_pld_borad.get(),en_pld_port.get(),0x07,data_len,data,0x87)
 
     if(ret == 1):
         # thread for recv
         test_run = 1
-        thread.start_new_thread(SERIAL.read_data,())  
-        pkt_offset = 0
-     
-        while test_run:  
-            time.sleep(1)  
-            print(SERIAL.message)
-            # parse message
-            while(pkt_offset < len(SERIAL.message)):
-                pkt_head = SERIAL.message[pkt_offset:1]
-                while head != '0x68':
-                    pkt_offset = pkt_offset + 1
-                    head = SERIAL.message[pkt_offset:1]
-
-                pkt_len =  SERIAL.message[pkt_offset+4:1]   
-                pkt_data = SERIAL.message[pkt_offset+6:pkt_len-5]
-                pkt_offset = pkt_offset + pkt_len
-
-            # save data  
-            with open(".\dc_current_data.txt",'a') as fp:   
-                for i in range(0,len(pkt_data)):
-                    fp.write(pkt_data[i:2]+',')  
+        _thread.start_new_thread(read_data_thd,(SERIAL, test_run))  
+        time.sleep(1)
+        _thread.start_new_thread(parse_save_thd,(SERIAL, test_run))  
 
 
-        # send stop cmd
-        data1 = []
-        ret = SERIAL.cmd_send_recv(en_pld_borad.get(),en_pld_port.get(),0x08,0,data1,0x88)
 
-        # close port
-        SERIAL.port_close()
 
 # button stop
 def button_stop():
     test_run = 0
     time.sleep(10)
+    # send stop cmd
+    data1 = []
+    ret = SERIAL.cmd_send_recv(en_pld_borad.get(),en_pld_port.get(),0x08,0,data1,0x88)
+
+    # close port
+    SERIAL.port_close()
+
+
     root.destroy()     
     print("test stop<<<")
 
