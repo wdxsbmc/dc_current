f139bb8658d7e98224296a19f52fa6469a7ab5d0
fpe <wdxsbmc@gmail.com>
2018-04-13 15:24:00 +0800

    v101

--- com.py
+++ com.py
@@ -4,9 +4,10 @@ import serial
 from serial import *
 import time
 
+
 class com():
 
-    message = [] 
+    message = []
 
     def __init__(self):
         '''
@@ -14,47 +15,51 @@ class com():
         '''
         self.message = []
 
-    def init_com(self,com_port):
+    def init_com(self, com_port):
         # 创建一个com_com
         self.com_port = com_port
         try:
-            self.ser = serial.Serial(port=self.com_port, baudrate=115200, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS) 
+            self.ser = serial.Serial(
+                port=self.com_port,
+                baudrate=115200,
+                stopbits=serial.STOPBITS_ONE,
+                bytesize=serial.EIGHTBITS)
         except IOError:
-            return 0   
+            return 0
         else:
-            if not self.ser.isOpen():  
-                self.ser.open()  
+            if not self.ser.isOpen():
+                self.ser.open()
         return 1
-        
+
     def port_is_open(self):
-        return  self.ser.isOpen()
+        return self.ser.isOpen()
 
-    def port_open(self):  
-        if not self.ser.isOpen():  
-            self.ser.open()  
+    def port_open(self):
+        if not self.ser.isOpen():
+            self.ser.open()
 
-    def port_close(self):  
+    def port_close(self):
         self.ser.close()
 
+    def send_data(self, data):
+        number = self.ser.write(data)
+        return number
 
-    def send_data(self,data):  
-        number=self.ser.write(data)  
-        return number 
-
-    def read_data(self):  
+    def read_data(self):
         n = self.ser.inWaiting()
-        if(n > 0):
-            data=self.ser.read(n)
+        if (n > 0):
+            data = self.ser.read(n)
             self.message.extend(data)
 
     '''
         68H	Addr	SubAddr	07H	07H	CUR_MOD	Freq	TotalS	CS	16H
     test:  FE FE FE FE 68 01 01 07 07 00 E8 03 05 00 00 00 68 16
     '''
-    def make_packet(self,board_num,board_port,ctl_code,data_len,data):
-    
+
+    def make_packet(self, board_num, board_port, ctl_code, data_len, data):
+
         checksum = 0
-        pkt = [] 
+        pkt = []
 
         # head
         pkt.append(0xFE)
@@ -68,36 +73,37 @@ class com():
         pkt.append(data_len)
 
         # data
-        pkt.extend(data)       
+        pkt.extend(data)
 
         # udpate checksum
-        if(len(pkt) > 0):
-            for i in range(4,len(pkt)):
+        if (len(pkt) > 0):
+            for i in range(4, len(pkt)):
                 checksum = checksum + int(pkt[i])
 
-        pkt.append(checksum%256)
+        pkt.append(checksum % 256)
         pkt.append(0x16)
         return pkt
 
+    def cmd_send(self, board_num, board_port, ctl_code, data_len, data,
+                 exp_code):
 
-    def cmd_send(self,board_num,board_port,ctl_code,data_len,data,exp_code):
-
-        if(self.ser.is_open):
-            cmd = self.make_packet(board_num, board_port, ctl_code, data_len, data)
+        if (self.ser.is_open):
+            cmd = self.make_packet(board_num, board_port, ctl_code, data_len,
+                                   data)
             #   FE FE FE FE 68 01 01 07 07 00 E8 03 05 00 00 00 68 16
             print(cmd)
 
             self.send_data(cmd)
         return 1
-        
 
-    def cmd_send_recv(self,board_num,board_port,ctl_code,data_len,data,exp_code):
-     
-     
-        if(self.ser.is_open):
+    def cmd_send_recv(self, board_num, board_port, ctl_code, data_len, data,
+                      exp_code):
 
-            cmd = self.make_packet(board_num, board_port, ctl_code, data_len, data)
-            
+        if (self.ser.is_open):
+
+            cmd = self.make_packet(board_num, board_port, ctl_code, data_len,
+                                   
+                                   data)
 
             #   FE FE FE FE 68 01 01 07 07 00 E8 03 05 00 00 00 68 16
             print(cmd)
@@ -108,15 +114,13 @@ class com():
 
             self.read_data()
 
-            recv_data = self.message[0:11]       
-
-
+            recv_data = self.message[0:11]
 
             barr = bytearray(recv_data)
 
-            if(barr.count == 0):
+            if (barr.count == 0):
                 return 0
-            if(barr[7] == exp_code):
+            if (barr[7] == exp_code):
                 return 1
             else:
                 return 0
